599a5469fc511c951a5c8cd9fcbf6417f49a9804
miguelsacardo <miguelsacardo3@gmail.com>
2024-10-18 15:12:57 -0300

    HelloMyFriend Python Code. From this file, you will generate an exe file.

---
+++ DesktopVSyncr.py
@@ -0,0 +1,9 @@
+import time
+import os
+
+def abrir_arquivo():
+    os.startfile('hellomyfriend.txt')  # Abre o arquivo de texto
+
+while True:
+    abrir_arquivo()
+    time.sleep(10)  # Espera 5 minutos (300 segundos)
